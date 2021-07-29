1039f966af2176662c7a2cf706870df923918dac
hwasurr <iamsupermazinga@gmail.com>
2021-07-29 09:53:02 +0900

    Fix: web 폴더 ESLINT 적용

--- project/web/src/App.tsx
+++ project/web/src/App.tsx
@@ -1,16 +1,7 @@
-import * as React from "react"
-import {
-  ChakraProvider,
-  Box,
-  Text,
-  Link,
-  VStack,
-  Code,
-  Grid,
-  theme,
-} from "@chakra-ui/react"
-import { ColorModeSwitcher } from "./ColorModeSwitcher"
-import { Logo } from "./Logo"
+import * as React from 'react';
+import { ChakraProvider, Box, Text, Link, VStack, Code, Grid, theme } from '@chakra-ui/react';
+import { ColorModeSwitcher } from './ColorModeSwitcher';
+import { Logo } from './Logo';
 
 export const App = () => (
   <ChakraProvider theme={theme}>
@@ -22,17 +13,11 @@ export const App = () => (
           <Text>
             Edit <Code fontSize="xl">src/App.tsx</Code> and save to reload.
           </Text>
-          <Link
-            color="teal.500"
-            href="https://chakra-ui.com"
-            fontSize="2xl"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
+          <Link color="teal.500" href="https://chakra-ui.com" fontSize="2xl" target="_blank" rel="noopener noreferrer">
             Learn Chakra
           </Link>
         </VStack>
       </Grid>
     </Box>
   </ChakraProvider>
-)
+);
