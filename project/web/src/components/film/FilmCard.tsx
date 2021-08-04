102586153b2764e5ba432274a72faf6e222e54bc
hwasurr <iamsupermazinga@gmail.com>
2021-08-04 19:14:39 +0900

    Add: 4장 상세페이지

--- project/web/src/components/film/FilmCard.tsx
+++ project/web/src/components/film/FilmCard.tsx
@@ -4,11 +4,13 @@ import {
   Heading,
   Image,
   LinkBox,
+  LinkOverlay,
   Stack,
   Text,
   useColorModeValue,
 } from '@chakra-ui/react';
 import React from 'react';
+import { Link } from 'react-router-dom';
 import { FilmsQuery } from '../../generated/graphql';
 
 interface FilmCardProps {
@@ -31,13 +33,15 @@ export default function FilmCard({ film }: FilmCardProps): React.ReactElement {
           </AspectRatio>
         </Box>
         <Stack>
-          <Heading
-            color={useColorModeValue('gray.700', 'white')}
-            fontSize="xl"
-            fontFamily="body"
-          >
-            {film.title}
-          </Heading>
+          <LinkOverlay as={Link} to={`/film/${film.id}`}>
+            <Heading
+              color={useColorModeValue('gray.700', 'white')}
+              fontSize="xl"
+              fontFamily="body"
+            >
+              {film.title}
+            </Heading>
+          </LinkOverlay>
           <Text fontSize="sm" color="gray.500" isTruncated>
             {film.subtitle ? film.subtitle : <>&nbsp;</>}
           </Text>
--- project/web/src/pages/Film.tsx
+++ project/web/src/pages/Film.tsx
@@ -1,7 +1,8 @@
-import { Box, Text, Spinner } from '@chakra-ui/react';
+import { Spinner, Text } from '@chakra-ui/react';
 import React from 'react';
 import { useParams } from 'react-router-dom';
 import CommonLayout from '../components/CommonLayout';
+import FilmDetail from '../components/film/FIlmDetail';
 import { useFilmQuery } from '../generated/graphql';
 
 interface FilmPageParams {
@@ -13,14 +14,16 @@ function Film(): React.ReactElement {
   const { data, loading, error } = useFilmQuery({
     variables: { filmId: Number(filmId) },
   });
+
   return (
     <CommonLayout>
       {loading && <Spinner />}
       {error && <Text>페이지를 표시할 수 없습니다.</Text>}
-
-      <Box>
-        <pre>{JSON.stringify(data, null, 2)}</pre>
-      </Box>
+      {filmId && data?.film ? (
+        <FilmDetail film={data.film} />
+      ) : (
+        <Text>페이지를 표시할 수 없습니다.</Text>
+      )}
     </CommonLayout>
   );
 }
