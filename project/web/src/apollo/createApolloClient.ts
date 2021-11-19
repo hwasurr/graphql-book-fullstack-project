1375ac750774825a72e53168aa721181773c353a
iamsupermazinga <iamsupermazinga@gmail.com>
2021-11-19 12:53:50 +0900

    Add: apollo client link

--- project/web/src/apollo/createApolloClient.ts
+++ project/web/src/apollo/createApolloClient.ts
@@ -1,31 +1,39 @@
-import { ApolloClient, NormalizedCacheObject } from '@apollo/client';
+import {
+  ApolloClient,
+  from,
+  HttpLink,
+  NormalizedCacheObject,
+} from '@apollo/client';
 import { onError } from '@apollo/client/link/error';
 import { createApolloCache } from './createApolloCache';
 
-const errorLink = onError(
-  ({ graphQLErrors, networkError, forward, operation }) => {
-    if (graphQLErrors) {
-      graphQLErrors.forEach(({ message, locations, path }) =>
-        // eslint-disable-next-line no-console
-        console.log(
-          `[GraphQL error]: -> ${operation.operationName} 
-        Message: ${message}, Query: ${path}, Location: ${JSON.stringify(
-            locations,
-          )}`,
-        ),
-      );
-    }
-
-    if (networkError) {
+const errorLink = onError(({ graphQLErrors, networkError, operation }) => {
+  if (graphQLErrors) {
+    graphQLErrors.forEach(({ message, locations, path }) =>
       // eslint-disable-next-line no-console
-      console.log(`[networkError]: -> ${operation.operationName}
+      console.log(
+        `[GraphQL error]: -> ${operation.operationName} 
+        Message: ${message}, Query: ${path}, Location: ${JSON.stringify(
+          locations,
+        )}`,
+      ),
+    );
+  }
+
+  if (networkError) {
+    // eslint-disable-next-line no-console
+    console.log(`[networkError]: -> ${operation.operationName}
     Message: ${networkError.message}`);
-    }
-  },
-);
+  }
+});
+
+const httpLink = new HttpLink({
+  uri: 'http://localhost:4000/graphql',
+});
 
 export const createApolloClient = (): ApolloClient<NormalizedCacheObject> =>
   new ApolloClient({
-    uri: 'http://localhost:4000/graphql',
     cache: createApolloCache(),
+    uri: 'http://localhost:4000/graphql',
+    link: from([errorLink, httpLink]),
   });
